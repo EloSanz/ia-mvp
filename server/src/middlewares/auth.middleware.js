dd279693302856dc360ba508d5d77c947ab10f21
devFernandoS <devtoolsfernando@gmail.com>
2025-10-18 18:47:30 -0300

    fix error en envio fallas de token

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -23,25 +23,19 @@ export const verifyToken = (token) => {
 
 export const authMiddleware = (req, res, next) => {
   try {
-    // Obtiene el token del encabezado Authorization
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (!token) {
-      return res.status(401).json({ message: 'Token no proporcionado.' });
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new AuthError('Token no proporcionado');
     }
 
-    // Verifica el token incluyendo la validez y expiración
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
-      if (err) {
-        // El error puede deberse a token expirado o inválido: deslogueamos y redirigimos al login
-        return res.status(401).json({ message: 'Error de tocken.' });
-      }
+    const token = authHeader.split(' ')[1]; // Bearer TOKEN
+    if (!token) {
+      throw new AuthError('Token no proporcionado');
+    }
 
-      // Si es válido, añade la información decodificada al objeto de request
-      req.user = decoded;
-      next();
-    });
+    const decoded = verifyToken(token);
+    req.userId = decoded.userId;
+    next();
   } catch (error) {
     next(error);
   }
