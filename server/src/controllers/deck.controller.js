ef33d0113f5b93faa3833a49b237b977d1845a54
devFernandoS <devtoolsfernando@gmail.com>
2025-10-20 23:17:35 -0300

    fix de stats mpckeados

--- server/src/controllers/deck.controller.js
+++ server/src/controllers/deck.controller.js
@@ -2,8 +2,6 @@ import { Deck } from '../models/deck.js';
 import { DeckDto } from '../dtos/deck.dto.js';
 import { BaseController } from './base.controller.js';
 import { ForbiddenError, NotFoundError } from '../utils/custom.errors.js';
-import fs from "fs";
-import { generateDeckCoverURL } from '../services/aiImage.service.js';
 
 export const DeckController = {
   /**
@@ -33,44 +31,44 @@ export const DeckController = {
    */
   createDeck: BaseController.wrap(async (req, res) => {
 
-  const { name, description, generateCover } = req.body;
-  const userId = parseInt(req.userId);
+    const { name, description, generateCover } = req.body;
+    const userId = parseInt(req.userId);
 
-  try {
+    try {
 
-    const deck = await Deck.create({
-      name,
-      description,
-      userId,
-      coverUrl: null
-    });
+      const deck = await Deck.create({
+        name,
+        description,
+        userId,
+        coverUrl: null
+      });
 
-    BaseController.success(res, deck, "Deck creado exitosamente", 201);
+      BaseController.success(res, deck, "Deck creado exitosamente", 201);
 
-    let coverBase64 = null;
+      let coverBase64 = null;
 
-    if (generateCover) {
-      (async () => {
-        const { generateDeckCoverBase64 } = await import("../services/aiImage.service.js");
-        const result = await generateDeckCoverBase64(name, description);
+      if (generateCover) {
+        (async () => {
+          const { generateDeckCoverBase64 } = await import("../services/aiImage.service.js");
+          const result = await generateDeckCoverBase64(name, description);
 
-        if (result.base64) {
-          coverBase64 = result.base64
-          await Deck.update(deck.id, { coverUrl: result.base64 });
-          // TODO: emitir evento con socket.io o notificación al front
-           console.log("Portada generada correctamente");
-        } else {
-          console.error("❌ Error al generar portada IA:", result.error);
-        }
-      })();
+          if (result.base64) {
+            coverBase64 = result.base64
+            await Deck.update(deck.id, { coverUrl: result.base64 });
+            // TODO: emitir evento con socket.io o notificación al front
+            console.log("Portada generada correctamente");
+          } else {
+            console.error("❌ Error al generar portada IA:", result.error);
+          }
+        })();
+      }
+
+
+    } catch (error) {
+      console.error("Error creating deck:", error);
+      throw error;
     }
 
-
-  } catch (error) {
-    console.error("Error creating deck:", error);
-    throw error;
-  }
-  
   }),
 
   /**
