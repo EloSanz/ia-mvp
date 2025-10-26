973c5a95dcd6f75821809d5625e37a6c3dce7452
devFernandoS <devtoolsfernando@gmail.com>
2025-10-26 19:56:53 -0300

    fix y configuracion para azure

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+envsubst '$API_UPSTREAM' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 # Default API upstream if not provided
 : ${API_UPSTREAM:=http://localhost:3000}
+# Make sure the variable is exported so envsubst can see it
+export API_UPSTREAM
+
+echo "[entrypoint] API_UPSTREAM=$API_UPSTREAM"
 
 # Render nginx template
 envsubst '$API_UPSTREAM' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
