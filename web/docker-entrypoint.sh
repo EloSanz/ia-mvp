3bdf0853f89b3c228b1f8dc5c5a9f6661ef5589c
devFernandoS <devtoolsfernando@gmail.com>
2025-11-15 15:08:39 -0300

    fix asignacion de vite p´por variable reasignable por azure

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -e
 
-envsubst '$VITE_API_URL' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
-# Default API upstream if not provided
-: ${VITE_API_URL:=http://localhost:3000}
+# Establece un valor por defecto para la URL del backend si no se proporciona.
+: ${NGINX_PROXY_URL:=http://localhost:3000}
 # Make sure the variable is exported so envsubst can see it
-export VITE_API_URL
+export NGINX_PROXY_URL
 
-echo "[entrypoint] VITE_API_URL=$VITE_API_URL"
+echo "[entrypoint] Configurando Nginx para redirigir /api a: $NGINX_PROXY_URL"
 
 # Render nginx template
-envsubst '$VITE_API_URL' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$NGINX_PROXY_URL' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx in foreground
 exec nginx -g 'daemon off;'
