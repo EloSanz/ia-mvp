fbf75cabb6e738b94439820c23addb2b45829125
devFernandoS <devtoolsfernando@gmail.com>
2025-11-25 00:45:32 -0300

    fix cambios prisma y contexto para modules lijeros

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-envsubst '$VITE_API_URL' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
-# Default API upstream if not provided
-: ${VITE_API_URL:=http://localhost:3000}
-# Make sure the variable is exported so envsubst can see it
-export VITE_API_URL
+# Set a default for VITE_API_URL if it's not provided and export it for envsubst
+export VITE_API_URL=${VITE_API_URL:-http://localhost:3000}
 
 echo "[entrypoint] VITE_API_URL=$VITE_API_URL"
 
-# Render nginx template
+# Render the nginx config template with the substituted environment variable
 envsubst '$VITE_API_URL' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
-# Start nginx in foreground
+# Start nginx in the foreground
 exec nginx -g 'daemon off;'
