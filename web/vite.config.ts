2c84821a75a2d09bf632306c3ff3df02893824a4
devFernandoS <devtoolsfernando@gmail.com>
2025-11-25 16:56:45 -0300

    entornos para deploy azrue

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true
-      }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      host: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:3000",
+          changeOrigin: true
+        }
+      },
+      allowedHosts: [
+        'icards-djfeb7c0cvdxhpav.canadacentral-01.azurewebsites.net',
+      ],
     }
-  }
+  };
 });
